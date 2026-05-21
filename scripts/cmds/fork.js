c787e8486fe17ffa3785e07ceba14ea8ab548c2f
mdsiyam121314151-source <mdsiyam121314151@gmail.com>
2026-05-21 23:36:58 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -32,15 +32,13 @@ module.exports = {
       // 🌟 Premium Loading Message
       const loading = await message.reply(`
 ╭━〔 ⚡ SYSTEM ⚡ 〕━╮
-┃
 ┃ ⏳ আরে মামা দাঁড়াও দিতেছি...
-┃
 ╰━━━━━━━━━━━━━━━━━━╯
 `);
 
       // 🎥 Google Drive Direct Video Link
       const videoUrl =
-        "https://drive.google.com/uc?id=1BC7jTudYN-6_BKks4GZwjK3CDFKNtftG";
+        "https://tmpfiles.org/dl/wzwLlgEiEgt1/upload_1779384931116.mp4";
 
       // download video
       const response = await axios.get(videoUrl, {
@@ -51,7 +49,7 @@ module.exports = {
 
       // ✅ Send video + text together
       await message.reply({
-        body: "এই নাও মামা হাত মারো 😎🔥",
+        body: "এই নাও মামা বসের ভয়েস ভিডিও 😎🔥",
         attachment: fs.createReadStream(filePath)
       });
 
